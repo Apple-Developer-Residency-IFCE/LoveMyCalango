d259a97a614af7752e9273d38abd88d7b7f18d15
Ravi Navarro <ravinavarro26@gmail.com>
2023-05-24 20:40:10 -0300

    initial commit

---
+++ loveMyPet/loveMyPet/App/loveMyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  loveMyPetApp.swift
+//  loveMyPet
+//
+//  Created by userext on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct loveMyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
