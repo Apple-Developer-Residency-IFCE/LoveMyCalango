78bc66ff47879024796669c1e8472e9fce35cfeb
raviNavarro1226 <ravi.navarro@madeinweb.app>
2023-05-27 21:20:33 -0300

    Empty Home

--- loveMyPet/loveMyPet/Modules/Home/View/HomeView.swift
+++ loveMyPet/loveMyPet/Modules/Home/View/HomeView.swift
@@ -15,9 +15,7 @@ struct HomeView: View {
         
         ScrollView(.vertical) {
             if viewModel.pets.isEmpty {
-                VStack {
-                    Text("Adicione um pet")
-                }
+                EmptyHome()
             } else {
                 NavigationView {
                     Grid {
@@ -35,27 +33,37 @@ struct HomeView: View {
     }
 }
 
-struct EmptyBgHome: View {
+struct EmptyHome: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "plus")
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(style: .init(lineWidth: 3))
                 .foregroundColor(Color("Gray-DBDBDA"))
-                .frame(width: 42, height: 42)
-                .overlay(content: {
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(style: .init(lineWidth: 3))
-                        .foregroundColor(Color("Gray-DBDBDA"))
-                })
+                .padding(.horizontal, 24)
+                .frame(height: 100)
+                .overlay {
+                    VStack {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .foregroundColor(Color("Gray-DBDBDA"))
+                            .frame(width: 42, height: 42)
+                            .padding()
+                    }
+                }
             Text(Constants.Home.emptyPets)
+                .foregroundColor(Color("Gray-B3B3B2"))
                 .multilineTextAlignment(.center)
+                .font(.custom(Constants.Font.Medium, size: 18))
+            Spacer()
         }
+        .padding(.top, 32)
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        EmptyBgHome()
+        HomeView()
     }
 }
 
