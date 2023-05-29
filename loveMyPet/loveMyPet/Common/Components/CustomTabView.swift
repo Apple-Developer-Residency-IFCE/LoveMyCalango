659fd6d5f1ce481912d5e1fdbf95b56d15042bde
IsaiasDimitri <isaiasdimitri2702@gmail.com>
2023-05-29 18:21:24 -0300

    Creating first attempt of tabview

---
+++ loveMyPet/loveMyPet/Common/Components/CustomTabView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabView.swift
+//  loveMyPet
+//
+//  Created by userext on 26/05/23.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabView()
+    }
+}
