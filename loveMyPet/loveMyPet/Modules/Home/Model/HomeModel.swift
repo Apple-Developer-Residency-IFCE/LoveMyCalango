016af308de6366a6a2e935295a40050383ea0f57
raviNavarro1226 <ravi.navarro@madeinweb.app>
2023-05-29 14:16:27 -0300

    Change getPets func

--- loveMyPet/loveMyPet/Modules/Home/Model/HomeModel.swift
+++ loveMyPet/loveMyPet/Modules/Home/Model/HomeModel.swift
@@ -1,18 +1,8 @@
 //
-//  HomeModel.swift
+//  HomeViewModel.swift
 //  loveMyPet
 //
 //  Created by userext on 24/05/23.
 //
 
 import Foundation
-import SwiftUI
-
-final class HomeViewModel: ObservableObject {
-    
-    @Published var pets = [Pet(id: UUID(), petName: "Carlos", petRace: "Carneiro", petImage: Image("AvatarDog1")),
-                           Pet(id: UUID(), petName: "Sergio", petRace: "Bode", petImage: Image("AvatarCat1")),
-                           Pet(id: UUID(), petName: "Joana", petRace: "Cabra", petImage: Image("AvatarCat2"))]
-    
-    private func getPets() {}
-}
--- loveMyPet/loveMyPet/Modules/Home/ViewModel/HomeViewModel.swift
+++ loveMyPet/loveMyPet/Modules/Home/ViewModel/HomeViewModel.swift
@@ -1,8 +1,24 @@
 //
-//  HomeViewModel.swift
+//  HomeModel.swift
 //  loveMyPet
 //
 //  Created by userext on 24/05/23.
 //
 
 import Foundation
+import SwiftUI
+
+final class HomeViewModel: ObservableObject {
+    
+    @Published var pets: [Pet] = []
+    
+    init() {
+        getPets()
+    }
+    
+    private func getPets() {
+        pets = [Pet(id: UUID(), petName: "Carlos", petRace: "Carneiro", petImage: Image("AvatarDog1")),
+                     Pet(id: UUID(), petName: "Sergio", petRace: "Bode", petImage: Image("AvatarCat1")),
+                     Pet(id: UUID(), petName: "Joana", petRace: "Cabra", petImage: Image("AvatarCat2"))]
+    }
+}
