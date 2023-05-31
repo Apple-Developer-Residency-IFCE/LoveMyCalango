527c08675262f1816dbb7d8566f19df5638832eb
Isaias Dimitri <isaiasdimitri2702@gmail.com>
2023-05-31 18:29:02 -0300

    Revert "Adding a modifier to apply against our HomeView."

--- loveMyPet/loveMyPet/Common/Extensions/Extensions.swift
+++ loveMyPet/loveMyPet/Common/Extensions/Extensions.swift
@@ -5,10 +5,4 @@
 //  Created by userext on 24/05/23.
 //
 
-import SwiftUI
-
-extension View {
-    func applyCustomHome(showing showingView: AnyView) -> some View {
-        modifier(CustomHomeNavigation(showingView: showingView))
-    }
-}
+import Foundation
