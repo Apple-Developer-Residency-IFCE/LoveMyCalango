071cf7f9f6f8bfb669b69a11cf61c7599cd0ede1
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2023-06-01 21:35:55 -0300

    moved fonts to Enums

--- loveMyPet/loveMyPet/Common/Enums/Constants.swift
+++ loveMyPet/loveMyPet/Common/Enums/Constants.swift
@@ -13,25 +13,5 @@ enum Constants {
         static let emptyPets = "Você não possui nenhum pet\n cadastrado :("
     }
     
-    enum Font {
-        static let ExtraLight = "Poppins-ExtraLight"
-        static let Thinitalic = "Poppins-Thinitalic"
-        static let ExtraLightitalic = "Poppins-ExtraLightitalic"
-        static let Bolditalic = "Poppins-Bolditalic"
-        static let Light = "Poppins-Light"
-        static let Medium = "Poppins-Medium"
-        static let SemiBolditalic = "Poppins-SemiBolditalic"
-        static let ExtraBolditalic = "Poppins-ExtraBolditalic"
-        static let ExtraBold = "Poppins-ExtraBold"
-        static let Blackitalic = "Poppins-Blackitalic"
-        static let Regular = "Poppins-Regular"
-        static let Lightitalic = "Poppins-Lightitalic"
-        static let Bold = "Poppins-Bold."
-        static let Black = "Poppins-Black"
-        static let Thin = "Poppins-Thin"
-        static let SemiBold = "Poppins-SemiBold"
-        static let Italic = "Poppins-Italic"
-        static let MediumItalic = "Poppins-MediumItalic"
-    }
 }
 
