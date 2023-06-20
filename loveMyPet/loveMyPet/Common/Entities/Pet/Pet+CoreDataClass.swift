f8cd280bf92341ff7b4b93842ce29b3214eb9f8b
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2023-06-20 15:27:01 -0300

    add core data pet model

---
+++ loveMyPet/loveMyPet/Common/Entities/Pet/Pet+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pet+CoreDataClass.swift
+//  loveMyPet
+//
+//  Created by Rodrigo Mendes on 20/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pet)
+public class Pet: NSManagedObject {
+
+}
