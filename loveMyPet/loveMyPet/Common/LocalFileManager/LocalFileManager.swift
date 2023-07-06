37229189ee8167470129d9a722c056e5edfe93f3
Vitor-costa-diniz <vitorcostadevv@gmail.com>
2023-07-05 21:34:06 -0300

    initial setup, and find source material
    Material para estudo de implementação no LocalFileManager

---
+++ loveMyPet/loveMyPet/Common/LocalFileManager/LocalFileManager.swift
@@ -0,0 +1,26 @@
+//
+//  FileManager.swift
+//  loveMyPet
+//
+//  Created by Vitor Costa on 05/07/23.
+//
+
+import SwiftUI
+
+class LocalFileManager {
+    static let instance = LocalFileManager()
+    private init() { }
+    func saveImage(image: UIImage) {
+        let data = image
+    }
+}
+
+// https://img.ly/blog/working-with-large-video-and-image-files-on-ios-with-swift/
+
+//https://cocoacasts.com/fm-2-how-to-store-an-image-in-the-documents-directory-in-swift
+
+//https://stackoverflow.com/questions/64546319/how-do-i-load-this-image-data-from-disk-and-present-it-in-my-swiftui-list
+
+//https://www.vadimbulavin.com/how-to-save-images-and-videos-to-core-data-efficiently/
+
+
