21aa73946388f07e35af4494c1511a9ec5e7d993
Tiago Vargas <tiago.ce10@outlook.com>
2023-07-17 19:18:15 -0300

    Add preview for `CuriosityCard`

--- loveMyPet/loveMyPet/Common/Components/CuriosityCard.swift
+++ loveMyPet/loveMyPet/Common/Components/CuriosityCard.swift
@@ -38,3 +38,8 @@ struct CuriosityCard: View {
     }
 }
 
+struct CuriosityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CuriosityCard(title: "Curiosidade do dia!", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus vehicula tortor, varius pharetra nibh pharetra id. Sed id quam non nunc mollis condimentum eu et ligula. Aenean id ex lectus. Donec fermentum dui elit, id tincidunt nunc accumsan in. Quisque vestibulum lobortis sagittis. Vestibulum arcu felis, tincidunt in ex ac, cursus faucibus dui. Mauris sit amet neque ac nisi hendrerit lacinia non in est. Curabitur condimentum orci in sagittis ultrices.")
+    }
+}
