e98576b12873a715c2adff9daf69ad20d738a693
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2023-07-24 15:04:41 -0300

    Add binding sheet boolean to EmptyListView button

--- loveMyPet/loveMyPet/Common/Components/EmptyListView.swift
+++ loveMyPet/loveMyPet/Common/Components/EmptyListView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct EmptyListView: View {
     var type: EmptyType
+    @Binding var addSheet: Bool
 
     var body: some View {
         VStack {
             Spacer()
             VStack(spacing: 22) {
                 Image(Assets.Image.lazyCat)
+
                 VStack {
                     Text(type == .pets ? Constants.Home.emptyPetSemibold : Constants.Task.emptyTaskSemibold)
                         .font(.custom(Font.SemiBold, size: 16))
@@ -24,24 +26,29 @@ struct EmptyListView: View {
                 .foregroundColor(Color(CustomColor.FontColor))
                 .multilineTextAlignment(.center)
                 .frame(width: 310, alignment: .top)
-                HStack {
-                    Image(systemName: "plus")
-                    Text(type == .pets ? Constants.Home.addPetTitle : Constants.Task.addTaskTitle)
+
+                Button {
+                    addSheet.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text(type == .pets ? Constants.Home.addPetTitle : Constants.Task.addTaskTitle)
+                    }
+                    .font(.custom(Font.SemiBold, size: 16))
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 40)
+                    .background(Color(CustomColor.MainColor))
+                    .foregroundColor(Color(CustomColor.FontColor))
+                    .cornerRadius(8)
                 }
-                .font(.custom(Font.SemiBold, size: 16))
-                .padding(.vertical, 16)
-                .padding(.horizontal, 40)
-                .background(Color(CustomColor.MainColor))
-                .foregroundColor(Color(CustomColor.FontColor))
-                .cornerRadius(8)
             }
             Spacer()
-        }
+        }.frame(maxWidth: .infinity)
     }
 }
 
 struct EmptyListView_Previews: PreviewProvider {
     static var previews: some View {
-        EmptyListView(type: .pets)
+        EmptyListView(type: .pets, addSheet: .constant(false))
     }
 }
