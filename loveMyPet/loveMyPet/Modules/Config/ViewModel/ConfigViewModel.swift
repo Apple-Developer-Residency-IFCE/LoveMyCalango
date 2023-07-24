c7b7bcf9f9eb8a5dea6ed1c11bac88c94febbd17
Vitor-costa-diniz <vitorcostadevv@gmail.com>
2023-07-24 18:17:17 -0300

    create func to verify with notifications is enable and Alert send the user to cellphone config

--- loveMyPet/loveMyPet/Modules/Config/ViewModel/ConfigViewModel.swift
+++ loveMyPet/loveMyPet/Modules/Config/ViewModel/ConfigViewModel.swift
@@ -11,6 +11,11 @@ final class ConfigViewModel: ObservableObject {
     @Published var isON = false
     private(set) var selectedScheme: Int?
     @AppStorage("preferredColor") var preferredColor: AppColorScheme = .system
+    @State private var isNotificationEnabled = false
+
+    init() {
+        checkNotificationPermission()
+    }
 
     func changeScheme(index: Int?) {
         switch index {
@@ -26,17 +31,46 @@ final class ConfigViewModel: ObservableObject {
     }
 
     func allowNotification() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge,
-            .sound]) { success, error in
-            if success {
-                Alert(
-                    title: Text("Notificação"),
-                    message: Text("Esta é uma notificação de exemplo."),
-                    dismissButton: .default(Text("Ok"))
-                )
-            } else if let error = error {
-                print(error.localizedDescription)
+        if isNotificationEnabled {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge,
+                .sound]) { success, error in
+                if success {
+                    print("All set")
+                } else if let error = error {
+                    print(error.localizedDescription)
+                }
             }
+        } else {
+            self.showSettingsAlert()
         }
     }
+
+    private func checkNotificationPermission() {
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                DispatchQueue.main.async {
+                    if settings.authorizationStatus == .authorized {
+                        self.isNotificationEnabled = true
+                    } else {
+                        self.isNotificationEnabled = false
+                    }
+                }
+            }
+        }
+
+    private func showSettingsAlert() {
+        let alert = UIAlertController(title: "Notificacões estão desativadas",
+        message: "Para ativar as notificações, vá para Settings > Notifications e permita notificações para esse app.",
+        preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            self.isON = false
+        })
+        alert.addAction(UIAlertAction(title: "Settings", style: .default) { _ in
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl)
+            }
+        })
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
 }
+
