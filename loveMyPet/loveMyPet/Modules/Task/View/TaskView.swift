2251e9cdb825145451a7131c61c62e02ff38b890
Tiago Vargas <tiago.ce10@outlook.com>
2023-07-25 18:50:13 -0300

    Show pending and completed tasks

--- loveMyPet/loveMyPet/Modules/Task/View/TaskView.swift
+++ loveMyPet/loveMyPet/Modules/Task/View/TaskView.swift
@@ -30,7 +30,17 @@ struct TaskView: View {
                 } else {
                     List {
                         Section {
-                            //
+                            ForEach(viewModel.tasks.filter { $0.isDone == false }) { task in
+                                Card {
+                                    HStack {
+                                        Information(title: task.title, category: task.type, alarm: task.time)
+
+                                        Spacer()
+
+                                        NameAndProfilePicture(pet: task.pet)
+                                    }
+                                }
+                            }
                         } header: {
                             Text("Tarefas Pendentes")
                                 .font(.title)
@@ -39,7 +49,17 @@ struct TaskView: View {
                         }
 
                         Section {
-                            //
+                            ForEach(viewModel.tasks.filter { $0.isDone }) { task in
+                                Card {
+                                    HStack {
+                                        Information(title: task.title, category: task.type, alarm: task.time)
+
+                                        Spacer()
+
+                                        NameAndProfilePicture(pet: task.pet)
+                                    }
+                                }
+                            }
                         } header: {
                             Label {
                                 Text("Tarefas concluídas")
@@ -78,6 +98,56 @@ struct Card<Content: View>: View {
                     .fill(Color(CustomColor.PetBackground))
             )
             .padding(.vertical, 4)
+    }
+}
+
+private struct Information: View {
+    var title: String
+    var category: TaskType
+    var alarm: Date
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(title)
+                .font(.title2)
+                .bold()
+            Text(category.rawValue)
+            AlarmTime(alarm: alarm)
+        }
+    }
+}
+
+private struct NameAndProfilePicture: View {
+    var pet: NewPet
+
+    var body: some View {
+        VStack {
+            ProfilePicture(pet: pet)
+            Text(pet.name)
+                .fontWeight(.bold)
+        }
+    }
+}
+
+private struct ProfilePicture: View {
+    var pet: NewPet
+
+    var body: some View {
+        Image(uiImage: UIImage(data: pet.image) ?? UIImage(named: Assets.Image.logo) ?? UIImage())
+            .resizable()
+            .frame(width: 64, height: 64)
+            .clipShape(Circle())
+    }
+}
+
+private struct AlarmTime: View {
+    let alarm: Date  // We'll only need the time, though
+
+    var body: some View {
+        HStack {
+            Image("IconClock")
+            Text(alarm, style: .time)
+        }
     }
 }
 
